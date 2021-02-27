7ba539c72ee0202a1a2d74a5a0f394d6494c758d
andrey548 <andreyverh@icloud.com>
2021-02-26 21:41:54 -0500

    commit

--- app/src/main/java/com/example/flixter/models/Movie.java
+++ app/src/main/java/com/example/flixter/models/Movie.java
@@ -6,20 +6,29 @@ import com.facebook.stetho.inspector.jsonrpc.JsonRpcException;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
+    int movieId;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+    //empty constructor need by the Parcler library
+    public Movie(){};
 
     public Movie(JSONObject jsonObject) throws JsonRpcException, JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException, JsonRpcException {
@@ -40,5 +49,13 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
